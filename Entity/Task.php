201356b90453336350371ad6d5e11b83c5e21462
Sylvain Floury <sylvain.floury@althea-groupe.com>
2013-03-27 13:28:22 +0100

    Ajout de l'entité Alt87TodoBundle:Task.

---
+++ Entity/Task.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace Alt87\Bundle\TodoBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Task
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class Task
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var boolean
+     *
+     * @ORM\Column(name="complete", type="boolean")
+     */
+    private $complete;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Task
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set complete
+     *
+     * @param boolean $complete
+     * @return Task
+     */
+    public function setComplete($complete)
+    {
+        $this->complete = $complete;
+    
+        return $this;
+    }
+
+    /**
+     * Get complete
+     *
+     * @return boolean 
+     */
+    public function getComplete()
+    {
+        return $this->complete;
+    }
+    
+    /**
+     * Alias get complete
+     *
+     * @return boolean 
+     */
+    public function isComplete()
+    {
+        return $this->complete;
+    }
+}
